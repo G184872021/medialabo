8799873b38996bb7061b0dcf2c9caa495516887e
G184872021 <218487@st.takushoku-u.ac.jp>
2023-11-05 20:27:02 +0900

    編集

--- 歴代総長スライドショー2/js/歴代総長スライドショーv2マスター.js
+++ 歴代総長スライドショー2/js/歴代総長スライドショーv2マスター.js
@@ -43,23 +43,3 @@ function updateThumbnails(){
     thumbnails[count-1].classList.add("selected");
 }
 
-/*
-
-const photos=[
-   
-    "https://www.takushoku-u.ac.jp/summary/images/summary_successive-chancellor_img_01~09.jpg",
-    "https://www.takushoku-u.ac.jp/summary/images/summary_successive-chancellor_img_10~20.jpg"
-];
-var num=0;
-function next() {
-    
-    //console.log(p.src);
-    const p=document.getElementById("photo");
-    num++;
-    if(num >= photos.length){
-        num = 0;
-    }
-    p.src = photos[num];
-
-}
-*/
