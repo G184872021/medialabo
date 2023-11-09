8a89a61648c3f53d79ddd2974b80c47022b6791e
G184872021 <218487@st.takushoku-u.ac.jp>
2023-11-09 16:33:11 +0900

    編集

--- 歴代総長スライドショー3/js/slideShowV3.js
+++ 歴代総長スライドショー3/js/slideShowV3.js
@@ -31,3 +31,9 @@ function makeURL(num) {
     }
     return head + n + tail;
 }
+let thumbnailsElement = document.querySelector("div.thumbnails");
+
+let thumbnailsImges = thumbnailsElement.children;
+for(let i=0;i<thumbnailsImges.length;i++){
+    thumbnailsImges[i].style.opacity="1.0";
+}
