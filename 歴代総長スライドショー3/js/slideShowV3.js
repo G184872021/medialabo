117f81572cb99d0483128b29c4077a90d38b070f
G184872021 <218487@st.takushoku-u.ac.jp>
2023-11-09 16:16:04 +0900

    編集

---
+++ 歴代総長スライドショー3/js/slideShowV3.js
@@ -0,0 +1,33 @@
+let count = 0;
+let url;
+const main = document.querySelector("div>img");
+const thumb = document.querySelector("div.thumbnails");
+
+function next() {
+    let old = count;
+    count++;
+    count = count % 19;
+    url = makeURL(count);
+    main.setAttribute("src", url);
+}
+
+function prev() {
+    let old = count;
+    count--;
+    count = (19 + count) % 19;
+    url = makeURL(count);
+    main.setAttribute("src", url);
+}
+
+function makeURL(num) {
+    const head = "https://www.takushoku-u.ac.jp/summary/images/summary_successive-chancellor_img_";
+    const tail = ".jpg";
+    let n;
+    num++;
+    if (num < 10) {
+        n = "0" + num;
+    } else {
+        n = num;
+    }
+    return head + n + tail;
+}
