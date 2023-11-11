2f3333910050fe16417f7cd42fd64ebd49dca3c9
G184872021 <218487@st.takushoku-u.ac.jp>
2023-11-11 19:23:20 +0900

    編集

--- 歴代総長スライドショー3/slideShowV3.js
+++ 歴代総長スライドショー3/slideShowV3.js
@@ -9,6 +9,7 @@ function next() {
     count = count % 19;
     url = makeURL(count);
     main.setAttribute("src", url);
+    updateThumbnail(old, count);
 }
 
 function prev() {
@@ -17,6 +18,14 @@ function prev() {
     count = (19 + count) % 19;
     url = makeURL(count);
     main.setAttribute("src", url);
+    updateThumbnail(old, count);
+}
+
+function updateThumbnail(oldIndex, newIndex) {
+    // 古いサムネイルのクラスを削除
+    thumbnailsElement.children[oldIndex].classList.remove('selected');
+    // 新しいサムネイルにクラスを追加
+    thumbnailsElement.children[newIndex].classList.add('selected');
 }
 
 function makeURL(num) {
